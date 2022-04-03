ffde9dd203c8b36ee9d07da2a87528cb3b0f6313
cdaringe <cdaringe@gmail.com>
2022-04-03 13:11:22 -0700

    chore: add build push docker

--- rad.ts
+++ rad.ts
@@ -3,6 +3,11 @@ import type { Task, Tasks } from "https://deno.land/x/rad/src/mod.ts";
 const test: Task = `deno test -A --unstable --coverage=cov_profile --import-map=import_map.json $(fd .test.ts)`;
 const testWatch: Task = `${test} --watch`;
 
+const buildPushDocker: Task = [
+  "docker build --platform linux/arm64/v8 --progress=plain -t cdaringe/deno_emit_server .",
+  "docker push cdaringe/deno_emit_server",
+].join(" && ");
+
 const format: Task = `deno fmt`;
 const lint: Task = `deno lint`;
 const check: Task = {
@@ -20,4 +25,5 @@ export const tasks: Tasks = {
   ...{ lint, l: lint },
   ...{ check, c: check },
   ...{ test, t: test, testWatch, tw: testWatch },
+  ...{ buildPushDocker, b: buildPushDocker },
 };
