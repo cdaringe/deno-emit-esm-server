145f6015e3d509f0a238a8c96f548add67d2db03
cdaringe <cdaringe@gmail.com>
2022-10-15 11:53:03 -0700

    chore: add log start msg

--- src/bin.ts
+++ src/bin.ts
@@ -9,16 +9,18 @@ const isProduction = DENO_ENV === "production";
 
 assert(
   !isProduction || (isProduction && EMIT_SERVER_ORIGIN),
-  `EMIT_SERVER_ORIGIN required in production mode`,
+  `EMIT_SERVER_ORIGIN required in production mode`
 );
 
 const port = PORT ? parseInt(PORT) : 7777;
 
+const origin = EMIT_SERVER_ORIGIN || `http://localhost:${port}`;
 const app = createServer({
   cors: true,
   emitMiddlewareOptions: {
-    origin: EMIT_SERVER_ORIGIN || `http://localhost:${port}`,
+    origin,
     cacheEntryTimeout: 1_000 * 60 * 2,
   },
 });
+console.info(`Starting server on ${port} [origin: ${origin}]`);
 await app.listen({ port });
